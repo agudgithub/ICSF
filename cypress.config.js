77308b8415278fb5d5037c565d4affa79b70d8fb
AndresGarva <andresgarvagnate@gmail.com>
2025-09-19 19:29:00 -0300

    temp: Hardcodear credenciales para prueba de CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,19 @@
 const { defineConfig } = require('cypress');
-require('dotenv').config(); // Lee el archivo .env
+// require('dotenv').config(); // <-- Comenta o elimina esta línea, ya no la necesitas.
 
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.js',
     baseUrl: 'https://thinking-tester-contact-list.herokuapp.com',
-    // Pasa las variables a Cypress
+    // Pasa las variables a Cypress directamente
     env: {
-      CYPRESS_USER_EMAIL: process.env.CYPRESS_USER_EMAIL,
-      CYPRESS_USER_PASSWORD: process.env.CYPRESS_USER_PASSWORD,
+      // REEMPLAZA ESTO:
+      // CYPRESS_USER_EMAIL: process.env.CYPRESS_USER_EMAIL,
+      // CYPRESS_USER_PASSWORD: process.env.CYPRESS_USER_PASSWORD,
+
+      // CON ESTO:
+      CYPRESS_USER_EMAIL: 'agarva@gmail.com',
+      CYPRESS_USER_PASSWORD: '1234567',
     },
   },
 });
