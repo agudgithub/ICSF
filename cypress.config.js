db8a3e69df73a972a68eb9f675765ff63db1b577
AndresGarva <andresgarvagnate@gmail.com>
2025-09-22 18:42:19 -0300

    feat: modificacion para presentacion

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('cypress');
-// require('dotenv').config(); // <-- Comenta o elimina esta línea, ya no la necesitas.
+require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
@@ -10,10 +10,6 @@ module.exports = defineConfig({
       // REEMPLAZA ESTO:
       CYPRESS_USER_EMAIL: process.env.CYPRESS_USER_EMAIL,
       CYPRESS_USER_PASSWORD: process.env.CYPRESS_USER_PASSWORD,
-
-      //O USA ESTO PARA PRUEBAS LOCALES:
-      //CYPRESS_USER_EMAIL: 'agarva@gmail.com',
-      //CYPRESS_USER_PASSWORD: '1234567',
     },
   },
 });
